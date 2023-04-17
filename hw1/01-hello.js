41b74b9336db63744e0cfb47cbe1126987d89662
hj7-HarshitaJaiswal <hj7@pdx.edu>
2023-04-16 23:49:05 -0700

    Done hw1 and updated hw1 checklist.md

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Harshita Jaiswal, and I use she/her/hers pronouns. I have been a graduate student since Winter 2023. I am from India and completed my undergraduate studies in computer science. I am excited to enhance my web knowledge through this course. In my free time, I enjoy traveling and exploring city life. I also like to experiment with my cooking skills by fusing different cuisines together."
+  );
 }
